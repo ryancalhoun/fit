9f7871116996ab7060b861d6381ade625cff15f3
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2015-12-03 20:44:14 -0600

    trying to fix failing cppunit install

--- prereqs.sh
+++ prereqs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
- 	sudo apt-get install libcppunit-dev
+	sudo apt-get update -qq
+ 	sudo apt-get install -y libcppunit-dev
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	brew install cppunit
 fi
