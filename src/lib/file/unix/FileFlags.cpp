87d4b7c2625d9f67d29f9f9af2ce96d0bbc032c9
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2016-04-13 19:52:24 -0500

    Removed unix base flags, moved creation-related flags to mirror windows

--- src/lib/file/unix/FileFlags.cpp
+++ src/lib/file/unix/FileFlags.cpp
@@ -3,13 +3,11 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
-#ifdef __APPLE__
-  #define O_LARGEFILE 0
-#endif
+#define SET_BIT(outflags, inflags, myflag, realflag) { if(((inflags) & (myflag)) != 0) outflags |= (realflag); }
 
 int FileFlags::openFlags() const
 {
-	int cflags = O_LARGEFILE | O_CLOEXEC;
+	int cflags = 0;
 
 	if((_flags & File::Read) != 0 && (_flags & File::Write) != 0)
 		cflags |= O_RDWR;
@@ -18,8 +16,15 @@ int FileFlags::openFlags() const
 	else if((_flags & File::Write) != 0)
 		cflags |= O_WRONLY;
 
-	#define SET_BIT(outflags, inflags, myflag, realflag) { if(((inflags) & (myflag)) != 0) outflags |= (realflag); }
 	SET_BIT(cflags, _flags, File::Append, O_APPEND);
+
+	return cflags;
+}
+
+int FileFlags::createFlags() const
+{
+	int cflags = 0;
+
 	SET_BIT(cflags, _flags, File::Create, O_CREAT);
 	SET_BIT(cflags, _flags, File::CreateEx, O_EXCL);
 	SET_BIT(cflags, _flags, File::Truncate, O_TRUNC);
--- test/lib/file/FileFlagsTest.cpp
+++ test/lib/file/FileFlagsTest.cpp
@@ -5,11 +5,6 @@
 	#include <windows.h>
 #else
 	#include <fcntl.h>
-	#ifdef __APPLE__
-		#define BASE_FLAGS O_CLOEXEC
-	#else
-		#define BASE_FLAGS O_LARGEFILE|O_CLOEXEC
-	#endif
 #endif
 
 void FileFlagsTest::testOpenFlags()
@@ -20,14 +15,11 @@ void FileFlagsTest::testOpenFlags()
 	assert_equal(FILE_APPEND_DATA|FILE_WRITE_ATTRIBUTES|STANDARD_RIGHTS_WRITE|WRITE_OWNER, FileFlags(File::Append|File::Write).openFlags());
 	assert_equal(GENERIC_WRITE, FileFlags(File::Truncate).openFlags());
 #else
-	assert_equal(BASE_FLAGS|O_RDONLY, FileFlags(File::Read).openFlags());
-	assert_equal(BASE_FLAGS|O_WRONLY, FileFlags(File::Write).openFlags());
-	assert_equal(BASE_FLAGS|O_RDWR, FileFlags(File::Read|File::Write).openFlags());
-	assert_equal(BASE_FLAGS|O_WRONLY|O_APPEND, FileFlags(File::Append|File::Write).openFlags());
-	assert_equal(BASE_FLAGS|O_RDWR|O_APPEND, FileFlags(File::Append|File::Read|File::Write).openFlags());
-	assert_equal(BASE_FLAGS|O_CREAT, FileFlags(File::Create).openFlags());
-	assert_equal(BASE_FLAGS|O_CREAT|O_EXCL, FileFlags(File::Create|File::CreateEx).openFlags());
-	assert_equal(BASE_FLAGS|O_TRUNC, FileFlags(File::Truncate).openFlags());
+	assert_equal(O_RDONLY, FileFlags(File::Read).openFlags());
+	assert_equal(O_WRONLY, FileFlags(File::Write).openFlags());
+	assert_equal(O_RDWR, FileFlags(File::Read|File::Write).openFlags());
+	assert_equal(O_WRONLY|O_APPEND, FileFlags(File::Append|File::Write).openFlags());
+	assert_equal(O_RDWR|O_APPEND, FileFlags(File::Append|File::Read|File::Write).openFlags());
 #endif
 }
 
@@ -39,6 +31,10 @@ void FileFlagsTest::testCreateFlags()
 	assert_equal(OPEN_ALWAYS, FileFlags(File::Create).createFlags());
 	assert_equal(OPEN_EXISTING, FileFlags(0).createFlags());
 	assert_equal(TRUNCATE_EXISTING, FileFlags(File::Truncate).createFlags());
+#else
+	assert_equal(O_CREAT, FileFlags(File::Create).createFlags());
+	assert_equal(O_CREAT|O_EXCL, FileFlags(File::Create|File::CreateEx).createFlags());
+	assert_equal(O_TRUNC, FileFlags(File::Truncate).createFlags());
 #endif
 }
 
